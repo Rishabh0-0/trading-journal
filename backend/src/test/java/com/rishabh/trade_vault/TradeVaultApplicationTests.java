0405c25c6fcf0d0b47204bfc05462d8b15edc7fa
Rishabh0-0 <rajrishabh2002711@gmail.com>
2026-05-31 11:42:51 +0530

    chore: initialize spring boot project

---
+++ backend/src/test/java/com/rishabh/trade_vault/TradeVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabh.trade_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
