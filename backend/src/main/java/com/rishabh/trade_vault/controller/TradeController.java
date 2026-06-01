a9405d17785ad1371001db91da30e491e20d1788
Rishabh0-0 <rajrishabh2002711@gmail.com>
2026-06-01 19:23:10 +0530

    feat: add API endpoint to close trades

--- backend/src/main/java/com/rishabh/trade_vault/controller/TradeController.java
+++ backend/src/main/java/com/rishabh/trade_vault/controller/TradeController.java
@@ -3,11 +3,14 @@ package com.rishabh.trade_vault.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rishabh.trade_vault.dto.TradeCloseRequestDTO;
 import com.rishabh.trade_vault.dto.TradeRequestDTO;
 import com.rishabh.trade_vault.dto.TradeResponseDTO;
 import com.rishabh.trade_vault.mapper.TradeMapper;
@@ -46,5 +49,11 @@ public class TradeController {
         }
 
         return allTradeDtos;
+    }
+
+    @PutMapping("/{id}/close")
+    public TradeResponseDTO closeTrade(@PathVariable Integer id,
+            @RequestBody @Valid TradeCloseRequestDTO closeRequest) {
+        return tradeMapper.toDto(tradeService.closeTrade(id, closeRequest));
     }
 }
